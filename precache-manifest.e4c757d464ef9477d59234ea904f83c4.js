511c9fc973b3e1661ad790dfdc81966d022a3bb2
kantbtrue <shashikant1989yadav@gmail.com>
2020-06-08 10:58:21 +0530

    Updates

---
+++ precache-manifest.e4c757d464ef9477d59234ea904f83c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b9e5c61fa9269443d5162406f7536da",
+    "url": "/the-reckon/index.html"
+  },
+  {
+    "revision": "003a234c6d6c3f59b749",
+    "url": "/the-reckon/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7bd92dd9f1d9fc2e6ac5",
+    "url": "/the-reckon/static/css/main.89698c42.chunk.css"
+  },
+  {
+    "revision": "003a234c6d6c3f59b749",
+    "url": "/the-reckon/static/js/2.22bdc0ae.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/the-reckon/static/js/2.22bdc0ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bd92dd9f1d9fc2e6ac5",
+    "url": "/the-reckon/static/js/main.714e7669.chunk.js"
+  },
+  {
+    "revision": "37b00273f8c5ff33734b",
+    "url": "/the-reckon/static/js/runtime-main.f385cbd6.js"
+  }
+]);
